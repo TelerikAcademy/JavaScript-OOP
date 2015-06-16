fe2c3d2e78c409f292988a32b98e6e8ab8be9bdc
Doncho Minkov <DonchoMinkov@gmail.com>
2015-06-16 18:46:06 +0300

    Added homework for Closures and Scopes

---
+++ 02. Scopes and Closures/00. homework/tasks/task-1.js
@@ -0,0 +1,54 @@
+/* Task Description */
+/* 
+	*	Create a module for working with books
+		*	The module must provide the following functionalities:
+			*	Add a new book to category
+				*	Each book has unique name, author and ISBN
+			*	List all books
+				*	Books are sorted by ID
+				*	This can be done by author, by category or all
+			*	Add new category
+				*	Each category has a unique name 
+			*	List all categories
+				*	Categories are sorted by ID
+		*	Each book/catagory has a unique identifier (ID) that is a number greater than 1
+			*	When adding a book/category, the is generated automatically
+		*	Add validation everywhere, where possible
+			*	Book and category names must be between 2 and 100 characters, including letters, digits and special characters ('!', ',', '.', etc)
+			*	Unique params are Book name, Category name, and Book ISBN
+				* Book authors can be repeated
+			*	If something is not valid - throw Error
+*/
+function solve() {
+	var library = (function () {
+
+		function listBooks() {
+
+		}
+
+		function addBook() {
+
+		}
+
+		function listCategories() {
+
+		}
+
+		function addCategory() {
+
+		}
+
+		return {
+			books: {
+				list: listBooks,
+				add: addBook
+			},
+			categories: {
+				list: listCategories,
+				addCategory
+			}
+		};
+	} ());
+	return library;
+}
+module.exports = solve;
---
+++ 02. Scopes and Closures/00. homework/tests/tests-task-1.js
@@ -0,0 +1,9 @@
+/* globals describe, it */
+var Person = require('../tasks/task-1')();
+var expect = require('chai').expect;
+
+describe('Tests for Closures and Scopes in JavaScript', function () {
+	describe('"Task 1"', function () {
+		/* soon */
+	});
+});
