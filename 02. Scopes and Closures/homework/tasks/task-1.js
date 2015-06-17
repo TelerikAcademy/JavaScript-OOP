6304b85a2412b60f705fe1a711bbf096a9f2dc80
Doncho Minkov <DonchoMinkov@gmail.com>
2015-06-17 12:30:54 +0300

    Adding tests, fixing the assignment

--- 02. Scopes and Closures/homework/tasks/task-1.js
+++ 02. Scopes and Closures/homework/tasks/task-1.js
@@ -3,25 +3,28 @@
 	*	Create a module for working with books
 		*	The module must provide the following functionalities:
 			*	Add a new book to category
-				*	Each book has unique name, author and ISBN
+				*	Each book has unique title, author and ISBN
+				*	It must return the newly created book with assigned ID
 			*	List all books
 				*	Books are sorted by ID
 				*	This can be done by author, by category or all
 			*	Add new category
 				*	Each category has a unique name 
+				*	It must return the newly created category with assigned ID
 			*	List all categories
 				*	Categories are sorted by ID
 		*	Each book/catagory has a unique identifier (ID) that is a number greater than 1
 			*	When adding a book/category, the is generated automatically
 		*	Add validation everywhere, where possible
-			*	Book and category names must be between 2 and 100 characters, including letters, digits and special characters ('!', ',', '.', etc)
-			*	Unique params are Book name, Category name, and Book ISBN
+			*	Book title and category name must be between 2 and 100 characters, including letters, digits and special characters ('!', ',', '.', etc)
+			*	Author is any non-empty string
+			*	Unique params are Book title, Category name, and Book ISBN
 				* Book authors can be repeated
+			*	Book ISBN is an unique code that contains either 10 or 13 digits
 			*	If something is not valid - throw Error
 */
 function solve() {
 	var library = (function () {
-
 		function listBooks() {
 
 		}
--- 02. Scopes and Closures/homework/tests/tests-task-1.js
+++ 02. Scopes and Closures/homework/tests/tests-task-1.js
@@ -1,9 +1,76 @@
 /* globals describe, it */
-var Person = require('../tasks/task-1')();
+var library = require('../tasks/task-1')();
 var expect = require('chai').expect;
 
 describe('Tests for Closures and Scopes in JavaScript', function () {
-	describe('"Task 1"', function () {
-		/* soon */
+	var CONSTS = {
+		VALID: {
+			BOOK_TITLE: 'BOOK #',
+			BOOK_ISBN: {
+				TEN_DIGITS: '1234567890',
+				THIRTEEN_DIGITS: '1234567890123',
+			},
+			AUTHOR: 'John Doe'
+		},
+		INVALID: {
+			BOOK_TITLE: {
+				SHORT: 'B',
+				LONG: new Array(101).join('A')
+			},
+			AUTHOR: '',
+			BOOK_ISBN: '1234'	
+		}
+	};
+
+
+	describe('"Task 1: Library"', function () {
+		it('expect library to exist', function () {
+			expect(library).to.exist;
+		});
+		it('expect library.books to exist', function () {
+			expect(library.books).to.exist;
+		});
+
+		describe('library.books.add', function () {
+			it('expect to exist and to be a function', function () {
+				expect(library.books.add).to.exist;
+				expect(library.books.add).to.be.a('function');
+			});
+
+			it('expect to be returned with generated ID(number greater than 1), when title, ISBN and author are valid', function () {
+				var bookToAdd = {
+					title: CONSTS.VALID.BOOK_TITLE,
+					isbn: CONSTS.VALID.BOOK_ISBN.TEN_DIGITS,
+					author: CONSTS.VALID.AUTHOR
+				};
+				var book = library.books.add(bookToAdd);
+				expect(book.id).to.exist;
+				expect(book.id).to.be.a('number');
+				expect(book.id).to.be.at.least(1);
+				expect(book.title).to.equal(bookToAdd.title);
+				expect(book.isbn).to.equal(bookToAdd.isbn);
+				expect(book.author).to.equal(bookToAdd.author);
+			});
+
+			it('expect to throw, when with invalid title (short)', function () {
+				function test() {
+					library.books.add({
+						title: CONSTS.INVALID.BOOK_TITTE.SHORT,
+						isbn: CONSTS.VALID.BOOK_ISBN.TEN_DIGITS,
+						author: CONSTS.VALID.AUTHOR
+					});
+				}
+				expect(test).to.throw();
+			});
+		});
+
+		it('expect library.books.add to exist and to be a function', function () {
+			expect(library.books.add).to.exist;
+			expect(library.books.add).to.be.a('function');
+		});
+
+		it('expect library.categories to exist', function () {
+			expect(library.categories).to.exist;
+		});
 	});
 });
