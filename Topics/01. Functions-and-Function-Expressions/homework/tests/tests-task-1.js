5b2d31777b312982fccd30e9445837cba7bb4def
Alex Tsvetanov <alex.tsvetanov.2002@gmail.com>
2017-01-24 18:54:56 +0200

    Update tests-task-1.js

--- Topics/01. Functions-and-Function-Expressions/homework/tests/tests-task-1.js
+++ Topics/01. Functions-and-Function-Expressions/homework/tests/tests-task-1.js
@@ -1,5 +1,5 @@
 /* globals describe, it */
-var solve = require('../tasks/task-1');
+var solve = require('../tasks/task-1')();
 var expect = require('chai').expect;
 
 describe('Tests for "Task 1"', function () {
