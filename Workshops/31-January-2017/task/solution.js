28c14da5b0d3b15113e11f5a440ce664a754ab64
Kristiyan Tsaklev <cuki@cukii.tk>
2017-01-31 16:02:07 +0200

    Add solution template

---
+++ Workshops/31-January-2017/task/solution.js
@@ -0,0 +1,14 @@
+function solve() {
+	function getProduct(productType, name, price) {
+	}
+
+	function getShoppingCart() {
+	}
+
+	return {
+		getProduct: getProduct,
+		getShoppingCart: getShoppingCart
+	};
+}
+
+module.exports = solve();
